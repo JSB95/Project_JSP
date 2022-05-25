b64ad86c0ebebe56e29175d46c4d6f4809d0ea53
504 <504@DESKTOP-0MVNHLQ>
2022-05-25 17:36:50 +0900

    신덕용브랜치

--- team3/src/main/java/dao/BoardDao.java
+++ team3/src/main/java/dao/BoardDao.java
@@ -33,10 +33,45 @@ public class BoardDao extends Dao {
 		
 		return false;
 	}
+	
+public int gettotalrow( String key , String keyword  ) {
+		
+		// 만약에 작성자 요청이면 
+		//if( key.equals("mid") ) { key = "mno"; keyword = MemberDao.getmemberDao().getmno(keyword)+""; }
+		
+		String sql = null;
+		if( key.equals("") && keyword.equals("") ) { sql ="select count(*) from board";} //검색이 없을경우 
+		else { sql ="select count(*) from board where "+key+" like '%"+keyword+"%'";} // 검색이 있을경우
+		
+		try { ps = con.prepareStatement(sql); rs = ps.executeQuery(); 
+			if( rs.next() ) return rs.getInt(1); 
+		}
+		catch( Exception e ) { System.out.println( e );} return 0;
+	}
+	
 	// 2. 모든 게시물 출력
-	public ArrayList<Board> getboardlist() {
-		String sql = "";
+	public ArrayList<Board> getboardlist(int startrow,int listsize,String key,String keyword) {
+		ArrayList<Board> boardlist = new ArrayList<Board>();
+		String sql = "null";
+		//if( key.equals("mid") ) { key = "mno"; keyword = MemberDao.getmemberDao().getmno(keyword)+""; }
+		if( key.equals("") && keyword.equals("") ) { //검색이 없을경우 
+			sql = "select * from board order by bno desc limit "+startrow+","+listsize; /* limit 시작 인덱스 , 표시 개수 */
+		}else {
+			sql ="select * from board where "+key+" like '%"+keyword+"%' order by bno desc limit "+startrow+","+listsize;
+		}
 		try {
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				Board board = new Board(
+						rs.getInt(1),rs.getString(2),
+						rs.getString(3),rs.getInt(4),
+						rs.getString(5),rs.getInt(6),rs.getInt(7),
+						rs.getString(8),rs.getString(9)
+						);
+				boardlist.add(board);
+			}
+			return boardlist;
 			
 		} catch (Exception e) {System.out.println("모든 게시물 출력 오류" +e);}
 		return null;
