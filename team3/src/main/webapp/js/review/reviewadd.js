5c729eb92420f907914b18f90c4675f8638d2bf8
DESKTOP-N8P75DI\504 <aaa>
2022-05-25 18:15:14 +0900

    강의평쓰기

--- team3/src/main/webapp/js/review/reviewadd.js
+++ team3/src/main/webapp/js/review/reviewadd.js
@@ -21,13 +21,41 @@ function tableview(){
 				
 				
 				tr += 
-				'<div id="lecture">'+
-					'<span>'+jsonarray[i]["lname"]+'</span>'+
-					'<span>'+jsonarray[i]["lname"]+'</span><br>'+
-					'<span>'+jsonarray[i]["lname"]+'</span>'+
-					'<span>'+jsonarray[i]["lname"]+'</span>'+
-				'</div>'
+				'<div class="tableviewbox">'+
+				'<div class="row">'+
+				'	<div class="col-md-4" id="lecture">'+
+				'		<span>과목명</span>'+
+				'		<span>교수명</span><br>'+
+				'		<span>학점</span>'+
+				'		<span>이수구분</span>'+
+				'		<span>공과대학</span><br><br>'+
+				'	</div>'+
+				'	<div class="col-md-8">'+
+				'		과제: <br>'+
+				'		<button>없음</button><button>보통</button><button>많음</button><br>'+
+				'		시험횟수: <br>'+
+				'		<button>없음</button><button>한번</button><button>두번</button><button>세번이상</button><br>'+
+				'		조모임: <br>'+
+				'	<button>없음</button><button>보통</button><button>많음</button><br>'+
+				'		<br>'+
+				'	</div>'+
+				'</div>'+
+				'<div class="row">'+
+				'	<div class="col-md-4" >'+
+				'		총평<br>'+
+				'		<img alt="" src="img/별.png">'+
+				'		<img alt="" src="img/별.png">'+
+				'		<img alt="" src="img/별.png">'+
+				'		<img alt="" src="img/별.png">'+
+				'		<img alt="" src="img/별.png"><br>'+
+				'		<br>'+
+				'	</div>'+
+				'	<div class="col-md-8">'+
+				'		<textarea rows="10" cols="80"></textarea><br>'+
+				'	</div>'+
+			'	</div>'
+			'</div>'
 			}
 			
-			$("#lecture").html( tr );
+			$("#tableviewbox").html( tr );
 }
--- team3/src/main/webapp/review/reviewwrite.jsp
+++ team3/src/main/webapp/review/reviewwrite.jsp
@@ -48,34 +48,8 @@
 					<%}%>
 				</table>
 			</div><!-- 강의리스트 -->
-			<div class="tableviewbox">
-				<div class="row">
-					<div class="col-md-4" id="lecture">
-						<span>과목명</span>
-						<span>교수명</span><br>
-						<span>학점</span>
-						<span>이수구분</span>
-						<span>공과대학</span>
-						총평<br>
-						<img alt="" src="img/별.png">
-						<img alt="" src="img/별.png">
-						<img alt="" src="img/별.png">
-						<img alt="" src="img/별.png">
-						<img alt="" src="img/별.png"><br>
-						<br>
-					</div>
-					<div class="col-md-8">
-						과제: <br>
-						<button>없음</button><button>보통</button><button>많음</button><br>
-						시험횟수: <br>
-						<button>없음</button><button>한번</button><button>두번</button><button>세번이상</button><br>
-						조모임: <br>
-						<button>없음</button><button>보통</button><button>많음</button><br>
-						
-						
-						<textarea rows="10" cols="80"></textarea><br>
-					</div>
-				</div>
+			<div id="tableviewbox">
+				
 			</div><!-- 강의평 -->
 			<button type="submit">완료</button>
 		</div>
